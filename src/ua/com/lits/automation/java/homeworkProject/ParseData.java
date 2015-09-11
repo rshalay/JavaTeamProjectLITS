fa95ceb20b9e819b463f446ff4ac4e4fd87cdda1
Lilya <Lilya@Lilia>
2015-09-11 22:34:43 +0300

    Lilya changes

--- src/ua/com/lits/automation/java/homeworkProject/ParseData.java
+++ src/ua/com/lits/automation/java/homeworkProject/ParseData.java
@@ -1,5 +1,54 @@
 package ua.com.lits.automation.java.homeworkProject;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ParseData {
+	
+	 protected String surname;
+	 protected String post;
+	 protected Double salary;
+	
+	public String getSurname() {
+		  return surname;
+		 }
+
+		 public void setSurname(String surname) {
+		  this.surname = surname;
+		 }
+
+		 public String getPost() {
+		  return post;
+		 }
+
+		 public void setPost(String post) {
+		  this.post = post;
+		 }
+
+		 public Double getSalary() {
+		  return salary;
+		 }
+
+		 public void setSalary(Double salary) {
+			this.salary = salary;
+		}
+
+		@JsonCreator
+		 public ParseData(@JsonProperty("surname") String surname,@JsonProperty("post") String post,
+		   @JsonProperty("salary") Double salary) {
+		  this.surname = surname;
+		  this.post = post;
+		  this.salary = salary;
+		 } 
+		 
+		 @Override
+		 public String toString() {
+		  StringBuilder sb = new StringBuilder();
+		  sb.append("surname:\t" + surname + "\n");
+		  sb.append("post:\t\t" + post + "\n");
+		  sb.append("salary:\t\t" + salary + "\n");
+		  return sb.toString();
+		 }
 
 }
--- src/ua/com/lits/automation/java/homeworkProject/StartCompilingProgram.java
+++ src/ua/com/lits/automation/java/homeworkProject/StartCompilingProgram.java
@@ -1,10 +1,33 @@
 package ua.com.lits.automation.java.homeworkProject;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+
 public class StartCompilingProgram {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 		
-
+		InputStream input = new FileInputStream("infoOnStudents.json");
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		objectMapper.disable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		TypeFactory typeFactory = TypeFactory.defaultInstance();
+		List<ParseData> parseData = objectMapper.readValue(input,
+				typeFactory.constructCollectionType(ArrayList.class, ParseData.class));
+		
+		for (ParseData item : parseData) {
+			
+			System.out.println(item.getSurname() + "\t" + item.getPost() + "\t\t" + item.getSalary());
+		}
 	}
 
 }
