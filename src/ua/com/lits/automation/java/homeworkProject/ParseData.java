e4f9eeab1d24604f5277c5977b90a3ea42c0efeb
Юра <@slr>
2015-09-12 12:57:42 +0300

    Comparator commit

--- src/ua/com/lits/automation/java/homeworkProject/ParseData.java
+++ src/ua/com/lits/automation/java/homeworkProject/ParseData.java
@@ -1,54 +1,58 @@
 package ua.com.lits.automation.java.homeworkProject;
 
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ParseData {
-	
-	 protected String surname;
-	 protected String post;
-	 protected Double salary;
-	
+
+	protected String surname;
+	protected String post;
+	protected Double salary;
+
 	public String getSurname() {
-		  return surname;
-		 }
+		return surname;
+	}
 
-		 public void setSurname(String surname) {
-		  this.surname = surname;
-		 }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-		 public String getPost() {
-		  return post;
-		 }
+	public String getPost() {
+		return post;
+	}
 
-		 public void setPost(String post) {
-		  this.post = post;
-		 }
+	public void setPost(String post) {
+		this.post = post;
+	}
 
-		 public Double getSalary() {
-		  return salary;
-		 }
+	public Double getSalary() {
+		return salary;
+	}
 
-		 public void setSalary(Double salary) {
-			this.salary = salary;
-		}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
 
-		@JsonCreator
-		 public ParseData(@JsonProperty("surname") String surname,@JsonProperty("post") String post,
-		   @JsonProperty("salary") Double salary) {
-		  this.surname = surname;
-		  this.post = post;
-		  this.salary = salary;
-		 } 
-		 
-		 @Override
-		 public String toString() {
-		  StringBuilder sb = new StringBuilder();
-		  sb.append("surname:\t" + surname + "\n");
-		  sb.append("post:\t\t" + post + "\n");
-		  sb.append("salary:\t\t" + salary + "\n");
-		  return sb.toString();
-		 }
+	@JsonCreator
+	public ParseData(@JsonProperty("surname") String surname, @JsonProperty("post") String post,
+			@JsonProperty("salary") Double salary) {
+		this.surname = surname;
+		this.post = post;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("surname:\t" + surname + "\n");
+		sb.append("post:\t\t" + post + "\n");
+		sb.append("salary:\t\t" + salary + "\n");
+		return sb.toString();
+	}
+
+	public int compareTo(ParseData p) {
+		int result = this.surname.compareTo(p.surname);
+		return result;
+	}
 
 }
--- src/ua/com/lits/automation/java/homeworkProject/SortData.java
+++ src/ua/com/lits/automation/java/homeworkProject/SortData.java
@@ -1,5 +1,21 @@
 package ua.com.lits.automation.java.homeworkProject;
+import java.util.Comparator;
 
 public class SortData {
 
+	public class SortData implements Comparator<ParseData> {
+		public int compare(ParseData s1, ParseData s2) {
+			if (s1.getSurname().compareTo(s2.getSurname()) != 0) {
+				// System.out.println("");
+				return s1.getSurname().compareTo(s2.getSurname());
+			}
+			if (s1.getPost().compareTo(s2.getPost()) != 0) {
+				// System.out.println("");
+				return s1.getPost().compareTo(s2.getPost());
+			}
+			if (s1.getSalary().compareTo(s2.getSalary()) == 0)
+				System.out.println("");
+			return s1.getSalary().compareTo(s2.getSalary());
+		}
+	}
 }
